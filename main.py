d2a8a988b9016eb1d5bbeb9602504ca08e9ce93a
Juan Cruz <jc23729@gmail.com>
2021-01-01 19:35:48 +0000

    #Step1 installing Flask

---
+++ main.py
@@ -0,0 +1,15 @@
+#Step 1
+#Installing Flask
+
+""""
+#we"re gonna run these first 3 lines of code
+$ python3 -m venv venv
+$ source venv/bin/activate
+
+(venv)$ pip3 install flask
+
+... lots of stuff ...
+Successfully installed flask Werkzeug Jinja2 ...
+Cleaning up...
+
+"""
