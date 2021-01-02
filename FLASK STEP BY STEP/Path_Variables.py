a737b8f6f550f67ffc51527eb1c09f8ecf4b95be
Juan Cruz <jc23729@gmail.com>
2021-01-02 16:07:17 +0000

    passing a variable

--- FLASK STEP BY STEP/Path_Variables.py
+++ FLASK STEP BY STEP/Path_Variables.py
@@ -1,11 +1,27 @@
+
+
+## we can have access to that value of that variable 
+## so you want to wrap it in <...> brackets that turns it into a variable
+
 USERS = {
   "whiskey": "Whiskey The Dog",
   "spike": "Spike The Porcupine",
 }  
 
-@app.route('/user/<username>')
+@app.route('/user/<username>')## <username> is turned into a variable because its wrapped in brackets. 
 def show_user_profile(username):
     """Show user profile for user."""
 
     name = USERS[username]
     return f"<h1>Profile for {name}</h1>"
+
+## THIS WOULD GET AN ERROR
+@app.route('/r/subreddit')
+def show_subreddit():
+  return "THIS IS A SUBREDDIT"
+
+## THIS WOULD WORK, BECAUSE IT MATCHES THE PATTERN
+@app.route('/r/subreddit')
+def show_subreddit(subreddit):# you would have to pass subreddit variable name, or whatever you put in on top for it to work
+  return "THIS IS A SUBREDDIT"
+
